2c2046ed5e59786ded8cc261ebbb5e135e2acf58
Leonardo Figueredo <Leonardo.Figueredo@techandsolve.com>
2019-08-20 10:14:51 -0500

    Creando el reducer principal.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import store from './store';
+import { Provider } from 'react-redux';
+
 
 function App() {
   return (
+    <Provider store = {store}>
     <div className="container">
       <header >
         <h1 className="text-center" >Administrador de pacientes de veterinaria</h1>
@@ -18,6 +22,7 @@ function App() {
       
 
     </div>
+    </Provider>
   );
 }
 
